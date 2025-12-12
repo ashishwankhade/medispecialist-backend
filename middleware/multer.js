048f99e2dec7bef740f47b4a1e0da76a83dac97f
Ashish Wankhade <wankhadeashish1951@gmail.com>
2025-12-12 16:23:30 +0530

    image upload setup multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -13,26 +13,22 @@
 //     storage,
 // })
 
-// appointment_backend/config/multer.js
 
 
-import multer from 'multer';
+import multer from "multer";
 
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-  if (!file.mimetype.startsWith('image/')) {
-    return cb(new Error('Only image files allowed'), false);
+  if (!file.mimetype.startsWith("image/")) {
+    return cb(new Error("Only image files allowed"), false);
   }
   cb(null, true);
 };
 
-const upload = multer({
+export const upload = multer({
   storage,
   fileFilter,
-  limits: {
-    fileSize: 5 * 1024 * 1024 // 5 MB
-  }
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
 });
 
-export default upload;
